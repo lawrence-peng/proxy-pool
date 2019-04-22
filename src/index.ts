2f9b5907bbb64e6fb30b274c98478fc2a3798495
pengtianzi <pengskyzi@gmail.com>
2019-04-22 15:35:44 +0800

    exports ProxySupplier ProxyInfo

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
 import HttpProxyPool from './core/HttpProxyPool';
+import ProxySupplier from './core/ProxySupplier';
+import ProxyInfo from './core/ProxyInfo';
 module.exports = HttpProxyPool;
+module.exports = ProxySupplier;
+module.exports = ProxyInfo;
